4938ed20b22a9696a21c67e6505a722eb4436077
Dario Orta <yotoo2920@yahoo.com>
2015-11-24 16:33:21 -0500

    adding tokenOf type

---
+++ tokenizer/TokenOfType.h
@@ -0,0 +1,14 @@
+#ifndef TOKENOFTYPE_H
+#define TOKENOFTYPE_H
+
+enum TokenOfType
+{
+   IntValue, RealValue, NegValue // whole, decimal, a Negative type of value
+   PlusSign, SubstSign, MultSign, DivSign, // +,-,*,/, operators
+   LogValue, E_Value, PowerValue, RootValue, // Log, e^, x^n, (x)^(1/2) value
+   PIvalue, // constants (add more)  maybe avogadro's # etc
+   OpenPar, ClosePar, // open and close parenthesis
+   Error // token with an error to be added to syntax_error vector
+}
+
+#endif

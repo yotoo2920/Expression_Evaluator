56c4c2f3a042bc25adcd930a899591f6049d048d
Dario Orta <yotoo2920@yahoo.com>
2015-12-04 00:31:04 -0500

    same number class just more readable lol

--- Number.cpp
+++ Number.cpp
@@ -12,38 +12,14 @@ Number::Number()
     ///empty constructor
 }
 
-///-------------------------------------------------------------------------------
-void Number::power(int& mant ,int& power)
-{
-    int newNumber=mant;
-
-    for (int i=0; i<power-1; i++)
-    {
-      mant=mant*newNumber;
-    }
-
-}
-
-///------------------------------------------------------------------------------
-
-/*Number::Number(string &strTo) {
-
-        converStringToNumber(strTo);
-
-         mantissa=mants;
-        exponent=exp;
-        type="number";
-        }
-        */
-///------------------------------------------------------------------------------
 Number::Number(int mants ,int exp )
 {
-  mantissa=mants;
-  exponent=exp;
+  mantissa = mants;
+  exponent = exp;
   reduceNumber();
 }
 
-///--------------------------------------------------------------------------------
+
 void Number::printNumber()
 {
 
@@ -51,14 +27,14 @@ void Number::printNumber()
   {
     int ten=10;
     power(ten,exponent);
-	
+
     cout << mantissa/ten<<".";
-    int temp=ten;		
+    int temp=ten;
 	if(mantissa < 0)
 	{
 		mantissa = mantissa * -1;
 	}
-	
+
   while(temp/(mantissa%ten)>10)		//changed this from 9 to 10
   {
     cout << "0";
@@ -70,18 +46,18 @@ void Number::printNumber()
   else cout << mantissa;
 }
 
-///------------------------------------------------------------------------------------
+
 void Number::converStringToNumber(string str)
 {
 
   char c;
   int num1;
   int num2;
-  int ten=10;
+  int ten = 10;
 
-  if (str.find(".")==-1)
+  if (str.find(".") == -1)
   {
-    exponent=0;
+    exponent = 0;
   }
   else
   {
@@ -90,40 +66,46 @@ void Number::converStringToNumber(string str)
 
   }
 
-  for (int i=0; i<str.length();i++)
+  for (int i=0; i < str.length(); i++)
   {
     ten=10;
-    c=str.at(i);
-    num1=c-'0';
+    c = str.at(i);
 
-    num2=str.length()-i-1;
+    num1 = c - '0';
+    num2 = str.length() -i -1;
 
-    if (i==str.length()-1)
+    if (i == str.length() -1)
     {
-      ten=1;
+      ten = 1;
     }
+
     power(ten,num2);
-    num1=num1*ten;
+    num1 = num1 * ten;
 
-    mantissa=mantissa+ num1;
-
+    mantissa = mantissa + num1;
   } // end the for
-
 }
 
-///--------------------------------------------------------------------------------
+void Number::power(int& mant ,int& power)
+{
+  int newNumber = mant;
+  for (int i=0; i < power-1; i++)
+  {
+    mant = mant * newNumber;
+  }
+}
+
 void Number::reduceNumber()
 {
-  while(exponent>4)
+  while(exponent > 4) // this round the number to 4 decimal places
   {
-    mantissa=mantissa/10;
+    mantissa = mantissa / 10;
     exponent--;
   }
 
-  while (mantissa%10==0 && exponent > 0)
+  while (mantissa%10 == 0 && exponent > 0)
   {
-    mantissa=mantissa/10;
+    mantissa = mantissa / 10;
     exponent--;
   }
-
 }

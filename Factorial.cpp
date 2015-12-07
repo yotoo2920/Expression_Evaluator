e6954196894569fb88dabbbfd67db8e200c30fbf
Dario Orta <yotoo2920@yahoo.com>
2015-12-06 21:12:06 -0500

    adding comments

--- Factorial.cpp
+++ Factorial.cpp
@@ -12,13 +12,16 @@ Factorial::Factorial()
 
 Number* Factorial::getFactorial(Number* fact)
 {
+
+	// Start magnitude with 1, then multiply the mantissa by itself specified in fact
+	// Return number!
 	magnitude = 1;
 	for(int i = 1; i <= fact->mantissa; i++)
 	{
 		magnitude = magnitude * i;
 	}
 	decimal = 0;
-	
+
 	Number* zz = new Number(magnitude, decimal);
 	return zz;
 }
